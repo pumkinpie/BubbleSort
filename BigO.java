a3f9477b9c9530f1b5e3b2f56c2e05a3bda40b5d
pumkinpie <73189117+pumkinpie@users.noreply.github.com>
2020-11-30 23:53:54 -0500

    Add files via upload

---
+++ BigO.java
@@ -0,0 +1,17 @@
+
+public class BigO {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//O(N)
+		//O(1)
+		//O(log(N))
+		//O(N^2)
+		
+		String name = "Lucas";
+		System.out.println("Hello" + name); //O(1)
+
+	}
+
+}
